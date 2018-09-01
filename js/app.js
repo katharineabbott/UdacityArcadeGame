e1aa98f84b52253af752aefa9ee2e0e10952c68b
katharineabbott <katharinemabbott@gmail.com>
2018-08-31 20:15:50 -0700

    created an enemy

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-   
+    this.x = x;
+    this.y = y;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +15,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + 5;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -57,7 +59,10 @@ class Player {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-let allEnemies = []
+let allEnemies = [
+    new Enemy(2,5),
+    new Enemy(3,5)
+]
 
 // Place the player object in a variable called player
 
